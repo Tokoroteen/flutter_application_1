d9cb15df64f29559335a60d75fc498fd7548fd86
Kenta Shinotsuka <macowner@KentaShinotsukanoMacBook-Air.local>
2023-02-01 21:15:15 +0900

    chapter2 05

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,44 +23,30 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _flag = false;
 
+  // ボタンクリック時にフラグを切り替える
+  _click() async {
+    setState(() {
+      _flag = !_flag;
+    });
+  }
+
+  int _counter = 0;
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -82,6 +60,53 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedContainer(
+                  duration: const Duration(seconds: 3),
+                  width: _flag ? 100 : 50,
+                  height: _flag ? 50 : 100,
+                  padding: _flag
+                      ? const EdgeInsets.all(0)
+                      : const EdgeInsets.all(30),
+                  margin: _flag
+                      ? const EdgeInsets.all(0)
+                      : const EdgeInsets.all(30),
+                  transform: _flag ? Matrix4.skewX(0.0) : Matrix4.skewX(0.3),
+                  color: _flag ? Colors.blue : Colors.grey),
+              AnimatedSwitcher(
+                  duration: const Duration(seconds: 3),
+                  child: _flag
+                      ? const Text("なにもない")
+                      : const Icon(Icons.favorite, color: Colors.pink))
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedOpacity(
+                  opacity: _flag ? 0.1 : 1.0,
+                  duration: const Duration(seconds: 3),
+                  child: Text(
+                    "消える文字",
+                    style: Theme.of(context).textTheme.headline4,
+                  )),
+              AnimatedSize(
+                  duration: const Duration(seconds: 3),
+                  child: SizedBox(
+                      width: _flag ? 50 : 200,
+                      height: _flag ? 50 : 200,
+                      child: Container(color: Colors.purple))),
+              AnimatedAlign(
+                  duration: const Duration(seconds: 3),
+                  alignment: _flag ? Alignment.topLeft : Alignment.bottomRight,
+                  child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Container(color: Colors.green)))
+            ],
+          ),
           const Text("Hello"),
           const Text("World"),
           TextButton(
@@ -105,13 +130,21 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.blue,
                 size: 36.0,
               ),
+              Icon(FontAwesomeIcons.gift, color: Colors.teal)
             ],
-          )
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          if (_counter % 2 == 0)
+            const Text('偶数です',
+                style: TextStyle(fontSize: 20, color: Colors.red))
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {print("押したね？")},
-        child: const Icon(Icons.timer),
+        onPressed: _click,
+        child: const Icon(Icons.ac_unit_outlined),
       ),
       drawer: const Drawer(child: Center(child: Text("Drawer"))),
       endDrawer: const Drawer(child: Center(child: Text("endDrawer"))),
